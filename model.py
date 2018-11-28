7670e8c2b3599cf87da051e3881ba3cda0195ebf
ibnutriyuono <ibnutriyuono@gmail.com>
2018-11-28 18:34:17 +0700

    open datasets and resize it to 50x50

--- model.py
+++ model.py
@@ -2,3 +2,20 @@ import numpy as numpy
 import matplotlib.pyplot as plt
 import os
 import cv2
+
+DATADIR = "PetImages"
+CATEGORIES = ["Dog", "Cat"]
+
+for category in CATEGORIES :
+    path = os.path.join(DATADIR, category)
+    for img in os.listdir(path):
+        img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+        # plt.imshow(img_array, cmap="gray")
+        # plt.show()
+        break
+    break
+
+IMG_SIZE = 50
+new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+plt.imshow(new_array, cmap="gray")
+plt.show()
